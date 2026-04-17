e99f00049414b08dc91b665047ce73a34dd6d5bf
tottfiy <ivanko.lisenko@gmail.com>
2026-04-17 03:01:09 +0300

    Planned future work for Lab 3

---
+++ Repository/Interfaces/IProductRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Repositories.Interfaces
+{
+    public class IProductRepository
+    {
+
+    }
+}
---
+++ Services/DTO/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.DTO
+{
+    internal class ProductDto
+    {
+    }
+}
